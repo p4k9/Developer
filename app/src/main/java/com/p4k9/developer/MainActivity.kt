79fdcc4098e45d419ed7a1f0e126359b6dd0a860
Paul.Kenjerski <kenjerski@gmail.com>
2017-11-09 13:22:56 -0500

    lunch update

--- app/src/main/java/com/p4k9/developer/MainActivity.kt
+++ app/src/main/java/com/p4k9/developer/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.p4k9.developer
 
+import android.app.Dialog
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    private static final String TAG = "HomeActivity";:
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    /**
+     * BottomNavigationView Setup
+     */
+    private fun _setupBottomNavigationView(){
+
     }
 }
